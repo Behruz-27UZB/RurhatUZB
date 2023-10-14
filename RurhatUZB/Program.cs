62fc9c322f112887976b29dcbd4e04c4a6ffd402
DELL <DELL@DESKTOP-4NB2LCS>
2023-10-14 17:41:34 +0300

    Add project files.

---
+++ RurhatUZB/Program.cs
@@ -0,0 +1,25 @@
+﻿//int 
+//for(int i = 7; i >= 1; i-=2)
+//{
+//    for(int a = i;a >= 1; a--)
+//    {
+//        if (a == 1)
+//        Console.Write(" * ");
+//        else
+//        {
+//            Console.Write(" * ");
+//        }
+
+//    }
+//    Console.WriteLine(      );
+//}
+char[,] tuplam = new char[9, 7];
+
+for (int i = 0;i < 9; i++)
+{
+    for (int j = i; j < 7; j ++)
+    {
+        tuplam[j] = ("*");
+    }
+}
+Console.WriteLine(tuplam[j]);
